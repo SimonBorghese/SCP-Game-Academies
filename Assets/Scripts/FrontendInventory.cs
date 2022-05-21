cc11f61b6841474970e6b3bc19e87bf336a802c4
Just-Feeshy <58647349+Just-Feeshy@users.noreply.github.com>
2022-05-21 15:32:44 -0400

    I did a push

--- Assets/Scripts/FrontendInventory.cs
+++ Assets/Scripts/FrontendInventory.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class FrontendInventory : MonoBehaviour
 {
+    public GameObject slot;
+
     private Invetory invetory;
+    private List<GameObject> slots;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        slots = new List<GameObject>();
     }
 
     void Awake() {
         invetory = (Invetory)this.gameObject.GetComponent(typeof(Invetory));
-    }
 
-    //Panel makeSlot() {
-    //    return null;
-    //}
+        for(int i=0; i<invetory.maxSize; i++) {
+            GameObject placement = Instantiate(slot);
+            
+        }
+    } 
 
     // Update is called once per frame
     void Update()
