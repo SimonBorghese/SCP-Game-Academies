c0168f9468fc72cba099b2a57f5e0f2614e8df4e
Just-Feeshy <58647349+Just-Feeshy@users.noreply.github.com>
2022-05-21 13:47:00 -0400

    push

---
+++ Assets/Scripts/FrontendInventory.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrontendInventory : MonoBehaviour
+{
+    private Invetory invetory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Awake() {
+        invetory = (Invetory)this.gameObject.GetComponent(typeof(Invetory));
+    }
+
+    Panel makeSlot() {
+        return null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
