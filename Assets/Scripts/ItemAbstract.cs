e497823a00f1a90cce93e7142955a5068abef7f7
Simon <Azkoh@protonmail.com>
2022-05-21 11:53:46 -0400

    item

--- Assets/Scripts/ItemAbstract.cs
+++ Assets/Scripts/ItemAbstract.cs
@@ -2,38 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-abstract public class Item : MonoBehaviour
+public class Item : MonoBehaviour
 {
-    protected bool onGround;
-    protected float defaultY;
+    public string name;
+    public MeshFilter mesh;
 
-    private float elapsed;
-
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
-    }
-
-    void Awake() {
         this.gameObject.tag = "Item";
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(onGround) {
-            defaultY = this.gameObject.transform.position.y - Mathf.Sin(elapsed * Mathf.PI);
-            elapsed += Time.deltaTime;
-            this.gameObject.transform.Rotate(elapsed, 0.0f, 0.0f);
-            this.gameObject.transform.position = new Vector3(
-                this.gameObject.transform.position.x,
-                defaultY + Mathf.Sin(elapsed * Mathf.PI),
-                this.gameObject.transform.position.z
-            );
-        }else {
-            elapsed = 0;
-            defaultY = this.gameObject.transform.position.y;
-        }
     }
 }
